b01f3cc8a67b537cac8ab0a75a0656162c260914
Rein Van Imschoot <reinvanimschoot@gmail.com>
2019-02-16 20:46:28 +0100

    Add App Center

--- App.tsx
+++ App.tsx
@@ -1,31 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
+import Analytics from 'appcenter-analytics';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
+  componentDidMount() {
+    Analytics.trackEvent('Video clicked', { Category: 'Music', FileName: 'favorite.avi' });
+    // Crashes.generateTestCrash();
+  }
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-interface Props {}
-export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
